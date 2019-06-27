3a1140a2da36d6b73ec3f8000cf8318cbc6c3cea
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-27 08:12:36 +0000

    Automatically backed up by Learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -6,7 +6,7 @@ export class Webpage extends Component
 {
   render(){
     return(
-      <div>  
+      <div>
         <h1>"The world's coolest webpage"</h1>
         <FillerText />
         <FillerText />
